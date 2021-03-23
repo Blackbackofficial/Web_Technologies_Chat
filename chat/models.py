ab3c08a14becfc369381b3de413cbae37c6de229
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-03-24 02:10:32 +0300

    Change dirictory

--- chat/models.py
+++ chat/models.py
@@ -4,7 +4,7 @@ from chat.managers import UserManager, QuestionManager, TagManager
 
 
 class UserProfile(models.Model):
-    avatar = models.ImageField(null=True, blank=True, verbose_name=u"аватар", upload_to='chat/static/images/')
+    avatar = models.ImageField(null=True, blank=True, verbose_name=u"аватар", upload_to='static/images/')
     register_date = models.DateField(null=False, blank=True, auto_now_add=True, verbose_name=u"дата регистрации")
     rating = models.IntegerField(blank=True, default=0, verbose_name=u"рейтинг")
     user = models.OneToOneField(User, related_name='userprofile', null=False, verbose_name="user",
